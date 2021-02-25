c2cefb22ac61e33d35faf5828de3ae0418955121
Suneetha Pamarthi <suneethapamarthi08@gmail.com>
2021-02-25 16:34:40 +0530

    First Commit

---
+++ src/main/java/com/natsoft/StoredProcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.natsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoredProcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoredProcDemoApplication.class, args);
+	}
+
+}
